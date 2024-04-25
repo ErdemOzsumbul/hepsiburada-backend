052fb1b0be51965b3b84c94ff81582e6cc1a1266
ErdemOzsumbul <e.ozsumbul@gmail.com>
2024-04-25 23:19:09 +0300

    vercel synchronization

--- src/controller/picture.js
+++ src/controller/picture.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const fs = require("fs");
+
 module.exports = (req, res) => {
   console.log(231);
   const rawPath = `src/` + req.query.filename;
-  const filePath = path.resolve(".", rawPath);
+  const filePath = path.join(process.cwd(), rawPath);
   const imageBuffer = fs.readFileSync(filePath);
   res.setHeader("Content-Type", "image/jpg");
   res.send(imageBuffer);
