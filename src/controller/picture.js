a0ca7e4f2e2dad0d3381f18e9370bea1d1ffa8e7
ErdemOzsumbul <e.ozsumbul@gmail.com>
2024-04-25 23:00:25 +0300

    vercel synchronization

--- src/controller/picture.js
+++ src/controller/picture.js
@@ -1,22 +1,14 @@
 const path = require("path");
-const { dirname } = require("path");
+const fs = require("fs");
 module.exports = (req, res) => {
-  console.log(231);
-  const rawPath = req.query.filename;
-  console.log(rawPath);
-  const options = {
-    root: path.join(process.cwd(), "src"),
-    dotfiles: "deny",
-    headers: {
-      "x-timestamp": Date.now(),
-      "x-sent": true,
-    },
-  };
-  console.log(options.root, rawPath);
-  res.sendFile(rawPath, options, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(err.status).end();
-    }
+  const rawPath = "/src/" + req.query.filename;
+  var filePath = path.join(process.cwd(), rawPath);
+  var stat = fs.statSync(filePath);
+  res.writeHead(200, {
+    "Content-Type": "image/*",
+    "Content-Length": stat.size,
   });
+
+  var readStream = fs.createReadStream(filePath);
+  readStream.pipe(res);
 };
