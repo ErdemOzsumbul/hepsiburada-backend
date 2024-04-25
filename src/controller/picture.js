45e5dbe4b364bb8c45cb9cfb869df4cc2bea7cb6
ErdemOzsumbul <e.ozsumbul@gmail.com>
2024-04-25 22:26:17 +0300

    vercel synchronization

--- src/controller/picture.js
+++ src/controller/picture.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { dirname } = require("path");
 module.exports = (req, res) => {
   console.log(231);
-  const rawPath = dirname(require.main.path) + `src/` + req.query.filename;
+  const rawPath = dirname(require.main.path) + `/src/` + req.query.filename;
   console.log(rawPath);
   const options = {
     root: path.join(path.resolve()),
